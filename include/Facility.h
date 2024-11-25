e45376de1a8041643c3804e86e867a878ef14665
yair rocca <yairyair9812@gmail.com>
2024-11-25 21:06:47 +0200

    Updated Facility implementation

--- include/Facility.h
+++ include/Facility.h
@@ -20,7 +20,7 @@ enum class FacilityCategory
 class FacilityType
 {
 public:
-    FacilityType(const string &name, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score);
+    FacilityType(const string &name, const FacilityCategory category, const int price,  const int lifeQuality_score, const int economy_score, const int environment_score);
     const string &getName() const;
     int getCost() const;
     int getLifeQualityScore() const;
@@ -41,7 +41,7 @@ class Facility : public FacilityType
 {
 
 public:
-    Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price, const int lifeQuality_score, const int economy_score, const int environment_score);
+    Facility(const string &name, const string &settlementName, const FacilityCategory category, const int price,  const int lifeQuality_score, const int economy_score, const int environment_score);
     Facility(const FacilityType &type, const string &settlementName);
     const string &getSettlementName() const;
     const int getTimeLeft() const;
